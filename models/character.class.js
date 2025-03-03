0096c25b48b8f3c6ccae6c44420bb4491d6bb70c
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-03 22:43:40 +0100

    pepe peligrose moves

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
 
   height = 280;
   y = 155;
+  speed = 15;
   IMAGES_WALKING = [
     "img/2_character_pepe/2_walk/W-21.png",
     "img/2_character_pepe/2_walk/W-22.png",
@@ -20,16 +21,32 @@ class Character extends MovableObject {
   }
 
   animate() {
-    /* Aus dem imageCache wird das Bild geladen und diese Bilder zu dem aktuellen Bild gesetzt */
     setInterval(() => {
       if (this.world.keyboard.RIGHT) {
-        let i = this.currentImage % this.IMAGES_WALKING.length; /* % = Rest, let i = 0 % = 6; % bedeutet 0/6 => 0 Rest 0 bsp. mit 5: 5/6 = 0 Rest 5, bsp. 6/6 = 1, Rest 0, 7/6 = 1, Rest 1*/
+        this.x += this.speed;
+      }
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+      }
+    }, 1000 / 60);
+
+    /* Aus dem imageCache wird das Bild geladen und diese Bilder zu dem aktuellen Bild gesetzt */
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        /* walk animation */
+        let i =
+          this.currentImage %
+          this.IMAGES_WALKING
+            .length; /* % = Rest, let i = 0 % = 6; % bedeutet 0/6 => 0 Rest 0 bsp. mit 5: 5/6 = 0 Rest 5, bsp. 6/6 = 1, Rest 0, 7/6 = 1, Rest 1*/
         /* i = 0,1,2,3,4,5,6 und dann wieder 0 */
-        let path = this.IMAGES_WALKING[i]; /* % holt sich immer wieder das Bild nun, das ganze stürzt nie mehr ab  */
+        let path =
+          this.IMAGES_WALKING[
+            i
+          ]; /* % holt sich immer wieder das Bild nun, das ganze stürzt nie mehr ab  */
         this.img = this.imageCache[path];
         this.currentImage++; /* currentImage wird auf + erhöht */
       }
-    }, 100); /* Animation: Bild wird alle 100ms gewechselt damit es flüssig aussieht */
+    }, 50); /* Animation: Bild wird alle 100ms gewechselt damit es flüssig aussieht */
   }
 
   jump() {}
