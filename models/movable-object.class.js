4fc4f134db6eaa1b2df9970fe47f9ca7e51ea61d
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-06 09:44:57 +0100

    refactoring ctx

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -31,6 +31,18 @@ class MovableObject {
       new Image(); /* this.img = document.getElementbyID('image') <img id= "image"> (wäre das gleiche) */
     this.img.src = path;
   }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    ctx.beginPath();
+    ctx.lineWidth = "5";
+    ctx.strokeStyle = "blue";
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+  }
   /**
    *
    * @param {Array} arr - ['img/image1.png', 'img/image2.png', … ] /* durch diese Bildpfade durchiterieren und zum ImageCache hinzufügen
