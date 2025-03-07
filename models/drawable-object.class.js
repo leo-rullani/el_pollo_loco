db45f7c7c3e5f726d36c60a2738dff43b4b1f2c7
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-07 04:51:04 +0100

    created drawable-object-class for statusbars

---
+++ models/drawable-object.class.js
@@ -0,0 +1,40 @@
+class DrawableObject {
+    img;
+    imageCache = {}; /* JSON, das Bilder speichert, loadImages-Funktion basierend darauf erstellen */
+    currentImage = 0;
+    x = 120;
+    y = 280;
+    height = 150;
+    width = 100;
+
+
+
+
+    loadImage(path) {
+        this.img =
+          new Image(); /* this.img = document.getElementbyID('image') <img id= "image"> (wäre das gleiche) */
+        this.img.src = path;
+      }
+
+
+      draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+      }
+
+
+
+      /**
+   *
+   * @param {Array} arr - ['img/image1.png', 'img/image2.png', … ] /* durch diese Bildpfade durchiterieren und zum ImageCache hinzufügen
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+      /* dieses arr.forEach greift auf die 6 Bilder von Pepe Peligroso (Character) zu */
+      let img = new Image(); /* Variable mit neuem Bild wird angelegt */
+      img.src = path; /* Bild wird geladen in das Image-Objekt */
+      this.imageCache[path] = img;
+    }); /* wir machen das für mehrere Bilder und gehen somit durch alle Pfade durch, loadImages inkl. alle Bildpfade (bspw. von Charakter in den jeweiligen Klassen als Array hinzufügen  */
+  }
+    
+
+}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,13 +1,5 @@
 /* Schablone, welche sagt, welche Felder da drin sein sollen. Lässt sich in game.js integrieren bei Variablen-Definierung: let character = new MovableObject(); */
-class MovableObject {
-  x = 120;
-  y = 280;
-  img;
-  height = 150;
-  width = 100;
-  imageCache =
-    {}; /* JSON, das Bilder speichert, loadImages-Funktion basierend darauf erstellen */
-  currentImage = 0;
+class MovableObject extends DrawableObject {
   speed = 0.15;
   otherDirection = false;
   speedY= 0;
@@ -26,16 +18,6 @@ class MovableObject {
 
   isAboveGround(){
     return this.y < 180;
-  }
-
-  loadImage(path) {
-    this.img =
-      new Image(); /* this.img = document.getElementbyID('image') <img id= "image"> (wäre das gleiche) */
-    this.img.src = path;
-  }
-
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
   drawFrame(ctx) {
@@ -73,18 +55,6 @@ class MovableObject {
 
   isDead() {
     return this.energy == 0;
-  }
-  /**
-   *
-   * @param {Array} arr - ['img/image1.png', 'img/image2.png', … ] /* durch diese Bildpfade durchiterieren und zum ImageCache hinzufügen
-   */
-  loadImages(arr) {
-    arr.forEach((path) => {
-      /* dieses arr.forEach greift auf die 6 Bilder von Pepe Peligroso (Character) zu */
-      let img = new Image(); /* Variable mit neuem Bild wird angelegt */
-      img.src = path; /* Bild wird geladen in das Image-Objekt */
-      this.imageCache[path] = img;
-    }); /* wir machen das für mehrere Bilder und gehen somit durch alle Pfade durch, loadImages inkl. alle Bildpfade (bspw. von Charakter in den jeweiligen Klassen als Array hinzufügen  */
   }
 
   moveRight() {
