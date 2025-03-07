2ba48a57c47bfce2ef34ed20b98f852926f6304f
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-07 04:16:23 +0100

    isHurt-functions done

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -12,6 +12,8 @@ class MovableObject {
   otherDirection = false;
   speedY= 0;
   acceleration = 2.5;
+  energy = 100;
+  lastHit = 0;
 
   applyGravity(){
     setInterval(() => {
@@ -44,6 +46,33 @@ class MovableObject {
     ctx.rect(this.x, this.y, this.width, this.height);
     ctx.stroke();
   }
+  }
+
+  //character.isColliding(chicken);
+  isColliding(mo) {
+    return this.x + this.width > mo.x &&
+    this.y + this.height > mo.y &&
+    this.x < mo.x &&
+    this.y < mo.y + mo.height;
+  }
+
+  hit() {
+    this.energy -= 5;
+    if(this.energy < 0) {
+      this.energy = 0;
+    } else {
+      this.lastHit = new Date().getTime(); 
+    }
+  }
+
+  isHurt() {
+    let timepassed = new Date().getTime() - this.lastHit; // Difference in ms
+    timepassed = timepassed / 1000;
+    return timepassed < 1;
+  }
+
+  isDead() {
+    return this.energy == 0;
   }
   /**
    *
