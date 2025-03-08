b4fd1b18d2f44e51b2c4c48fac68f08179b4f143
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-08 01:17:12 +0100

    idle-long-idle function implementation for pepe peligroso

--- models/character.class.js
+++ models/character.class.js
@@ -1,9 +1,10 @@
 class Character extends MovableObject {
-  /* Character hat mit "extends" nun alle Eigenschaften, die MovableObject auch hat */
-
+  // Grundwerte
   height = 250;
   y = 70;
   speed = 10;
+
+  // Lauf-Animationen (bereits bekannt)
   IMAGES_WALKING = [
     "img/2_character_pepe/2_walk/W-21.png",
     "img/2_character_pepe/2_walk/W-22.png",
@@ -13,6 +14,7 @@ class Character extends MovableObject {
     "img/2_character_pepe/2_walk/W-26.png",
   ];
 
+  // Sprung-Animationen
   IMAGES_JUMPING = [
     "img/2_character_pepe/3_jump/J-31.png",
     "img/2_character_pepe/3_jump/J-32.png",
@@ -25,69 +27,146 @@ class Character extends MovableObject {
     "img/2_character_pepe/3_jump/J-39.png",
   ];
 
+  // Tot-Animationen
   IMAGES_DEAD = [
-    'img/2_character_pepe/5_dead/D-51.png',
-    'img/2_character_pepe/5_dead/D-52.png',
-    'img/2_character_pepe/5_dead/D-53.png',
-    'img/2_character_pepe/5_dead/D-54.png',
-    'img/2_character_pepe/5_dead/D-55.png',
-    'img/2_character_pepe/5_dead/D-56.png',
-    'img/2_character_pepe/5_dead/D-57.png'
+    "img/2_character_pepe/5_dead/D-51.png",
+    "img/2_character_pepe/5_dead/D-52.png",
+    "img/2_character_pepe/5_dead/D-53.png",
+    "img/2_character_pepe/5_dead/D-54.png",
+    "img/2_character_pepe/5_dead/D-55.png",
+    "img/2_character_pepe/5_dead/D-56.png",
+    "img/2_character_pepe/5_dead/D-57.png",
   ];
 
+  // Verletzungs-Animationen
   IMAGES_HURT = [
-    'img/2_character_pepe/4_hurt/H-41.png',
-    'img/2_character_pepe/4_hurt/H-42.png',
-    'img/2_character_pepe/4_hurt/H-43.png',
-  ]
+    "img/2_character_pepe/4_hurt/H-41.png",
+    "img/2_character_pepe/4_hurt/H-42.png",
+    "img/2_character_pepe/4_hurt/H-43.png",
+  ];
 
+  // Idle / Long-Idle (Platzhalter – bitte richtige Pfade eintragen!)
+  IMAGES_IDLE = [
+    "img/2_character_pepe/1_idle/idle/I-1.png",
+    "img/2_character_pepe/1_idle/idle/I-2.png",
+    "img/2_character_pepe/1_idle/idle/I-3.png",
+    "img/2_character_pepe/1_idle/idle/I-4.png",
+    "img/2_character_pepe/1_idle/idle/I-5.png",
+    "img/2_character_pepe/1_idle/idle/I-6.png",
+    "img/2_character_pepe/1_idle/idle/I-7.png",
+    "img/2_character_pepe/1_idle/idle/I-8.png",
+    "img/2_character_pepe/1_idle/idle/I-9.png",
+    "img/2_character_pepe/1_idle/idle/I-10.png"
+  ];
+
+  IMAGES_LONG_IDLE = [
+    "img/2_character_pepe/1_idle/long_idle/I-11.png",
+    "img/2_character_pepe/1_idle/long_idle/I-12.png",
+    "img/2_character_pepe/1_idle/long_idle/I-13.png",
+    "img/2_character_pepe/1_idle/long_idle/I-14.png",
+    "img/2_character_pepe/1_idle/long_idle/I-15.png",
+    "img/2_character_pepe/1_idle/long_idle/I-17.png",
+    "img/2_character_pepe/1_idle/long_idle/I-18.png",
+    "img/2_character_pepe/1_idle/long_idle/I-19.png",
+    "img/2_character_pepe/1_idle/long_idle/I-20.png"
+  ];
+
+  // Referenz auf die Welt, Zeitmesser
   world;
-  /* waling_sound = new Audio('audio/running.mp3') */
+  timeSinceLastMove = 0; // in ms (oder in "Frames", je nach Zählweise)
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
+
+    // Alle Bild-Arrays laden
     this.loadImages(this.IMAGES_WALKING);
     this.loadImages(this.IMAGES_JUMPING);
     this.loadImages(this.IMAGES_DEAD);
     this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_LONG_IDLE);
+
     this.applyGravity();
     this.animate();
   }
 
+  // Zusätzliche Variable zum „Verlangsamen“ der Idle-Animation
+  idleFrameCounter = 0;
+
   animate() {
+    // 1) Bewegung & Kamera (unverändert)
     setInterval(() => {
+      let noKeyPressed = true;
       if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
         this.moveRight();
         this.otherDirection = false;
+        noKeyPressed = false;
       }
       if (this.world.keyboard.LEFT && this.x > 0) {
         this.moveLeft();
         this.otherDirection = true;
+        noKeyPressed = false;
       }
-      
-      if (this.world.keyboard.SPACE && !this.isAboveGround()){
-        this.jump();  
+      if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+        this.jump();
+        noKeyPressed = false;
       }
-    
-      this.world.camera_x = -this.x + 100; /* Pepe läuft immer 100px vom linken Rand entfernt */
+      if (this.world.keyboard.D) {
+        noKeyPressed = false;
+      }      
+
+      if (noKeyPressed) {
+        this.timeSinceLastMove += 1000 / 60;
+      } else {
+        this.timeSinceLastMove = 0;
+      }
+
+      this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
 
-    /* Aus dem imageCache wird das Bild geladen und diese Bilder zu dem aktuellen Bild gesetzt */
+    // 2) Animationen (Bilder wechseln)
     setInterval(() => {
-
-      if(this.isDead()) {
+      if (this.isDead()) {
         this.playAnimation(this.IMAGES_DEAD);
-      } else if(this.isHurt()){
+      } else if (this.isHurt()) {
         this.playAnimation(this.IMAGES_HURT);
       } else if (this.isAboveGround()) {
         this.playAnimation(this.IMAGES_JUMPING);
-        } else {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-        /* walk animation */
-        this.playAnimation(this.IMAGES_WALKING);
+      } else {
+        // => auf dem Boden => Idle oder Laufen
+        this.playIdleOrWalking();
+      }
+    }, 50);
+  }
+
+  playIdleOrWalking() {
+    if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+      // Laufen => Normale Animation (alle 50ms)
+      this.playAnimation(this.IMAGES_WALKING);
+      // Reset für Idle-Frame-Zähler
+      this.idleFrameCounter = 0;
+    } else {
+      // => Idle- / Long-Idle-Phase
+      let t = this.timeSinceLastMove;
+
+      if (t > 10000) {
+        // Long Idle, alle 5 Frames (Beispiel)
+        this.playIdleAnimationSlow(this.IMAGES_LONG_IDLE, 5);
+      } else if (t > 5000) {
+        // Idle, alle 4 Frames
+        this.playIdleAnimationSlow(this.IMAGES_IDLE, 4);
+      } else {
+        // Weniger als 5s => auch Idle, aber evtl. etwas schneller (alle 3 Frames)
+        this.playIdleAnimationSlow(this.IMAGES_IDLE, 3);
       }
     }
-    }, 50); /* Animation: Bild wird alle 100ms gewechselt damit es flüssig aussieht */
+  }
+  playIdleAnimationSlow(images, skipFrames) {
+    this.idleFrameCounter++;
+    if (this.idleFrameCounter >= skipFrames) {
+      this.idleFrameCounter = 0;
+      this.playAnimation(images);
+    }
   }
 
   jump() {
