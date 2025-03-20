4093a6fca47a7b4b7b217fd4ca384417bb183c54
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-20 15:03:46 +0100

    smallchicken js js doc

--- models/smallchicken.class.js
+++ models/smallchicken.class.js
@@ -1,30 +1,85 @@
+/**
+ * Represents a smaller chicken enemy in the game, extending MovableObject.
+ * This chicken has lower damage, reduced size, and unique animations.
+ * @extends MovableObject
+ */
 class SmallChicken extends MovableObject {
+  /**
+   * The amount of damage this small chicken inflicts on the character.
+   * @type {number}
+   */
   damage = 5;
+
+  /**
+   * The Y-position of the small chicken on the canvas.
+   * @type {number}
+   */
   y = 390;
+
+  /**
+   * The width of the small chicken in pixels.
+   * @type {number}
+   */
   width = 40;
+
+  /**
+   * The height of the small chicken in pixels.
+   * @type {number}
+   */
   height = 35;
 
+  /**
+   * Array of image paths for the small chicken's walking animation.
+   * @type {string[]}
+   */
   IMAGES_WALKING = [
     "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
     "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
     "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
   ];
 
+  /**
+   * Array of image paths for the small chicken's dead animation.
+   * @type {string[]}
+   */
   IMAGES_DEAD = ["img/3_enemies_chicken/chicken_small/2_dead/dead.png"];
 
+  /**
+   * Interval ID for movement.
+   * @type {number|null}
+   */
   moveInterval;
+
+  /**
+   * Interval ID for walking animation.
+   * @type {number|null}
+   */
   walkInterval;
+
+  /**
+   * Interval ID for dead animation.
+   * @type {number|null}
+   */
   deadInterval;
 
+  /**
+   * Creates a new SmallChicken instance, loads its images,
+   * sets a random position and speed, and starts its animation.
+   */
   constructor() {
     super().loadImage("img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
     this.loadImages(this.IMAGES_WALKING);
     this.loadImages(this.IMAGES_DEAD);
+
     this.x = 200 + Math.random() * 5000;
-    this.speed = 0.3 + Math.random() * 0.6 ;
-    this.animate(); 
+    this.speed = 0.3 + Math.random() * 0.6;
+
+    this.animate();
   }
 
+  /**
+   * Starts intervals for movement (moving left) and animation (walking).
+   */
   animate() {
     this.moveInterval = setInterval(() => {
       this.moveLeft();
@@ -35,6 +90,10 @@ class SmallChicken extends MovableObject {
     }, 200);
   }
 
+  /**
+   * Plays the dead animation by clearing existing intervals and repeatedly
+   * showing the dead image at a fast interval.
+   */
   playDeadAnimation() {
     clearInterval(this.moveInterval);
     clearInterval(this.walkInterval);
@@ -45,6 +104,9 @@ class SmallChicken extends MovableObject {
     }, 10);
   }
 
+  /**
+   * Stops all active intervals (movement, walking, dead).
+   */
   stopIntervals() {
     if (this.moveInterval) {
       clearInterval(this.moveInterval);
@@ -60,8 +122,10 @@ class SmallChicken extends MovableObject {
     }
   }
 
+  /**
+   * Resumes walking and moving animations after they were stopped.
+   */
   resumeIntervals() {
     this.animate();
-  }  
-
+  }
 }
