a822352bbcfcff76a9291208ec2a71dce03c52cd
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-20 14:08:30 +0100

    cloud.js - jsdoc

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,26 +1,57 @@
+/**
+ * Represents a cloud in the game world, extending MovableObject.
+ * Clouds are placed in the background and slowly move left.
+ * @extends MovableObject
+ */
 class cloud extends MovableObject {
+  /**
+   * The Y-position of the cloud.
+   * @type {number}
+   */
   y = 20;
-  width = 500;
-  height = 250; 
-  speed = 0.15
 
+  /**
+   * The width of the cloud image.
+   * @type {number}
+   */
+  width = 500;
+
+  /**
+   * The height of the cloud image.
+   * @type {number}
+   */
+  height = 250;
+
+  /**
+   * The horizontal speed at which the cloud moves to the left.
+   * @type {number}
+   */
+  speed = 0.15;
+
+  /**
+   * Creates a new cloud instance, loads its image,
+   * positions it randomly on the X-axis, and starts its movement.
+   */
   constructor() {
     super().loadImage("img/5_background/layers/4_clouds/1.png");
-
-    this.x =
-      0 +
-      Math.random() *
-        5000;
+    this.x = 0 + Math.random() * 5000;
     this.animate();
   }
 
-  animate(){
+  /**
+   * Triggers the movement of the cloud.
+   */
+  animate() {
     this.moveLeft();
   }
 
-  moveLeft(){
+  /**
+   * Moves the cloud to the left at the defined speed.
+   * Uses setInterval to update the position continuously.
+   */
+  moveLeft() {
     setInterval(() => {
-        this.x -= this.speed;
-    }, 1000/60);
-    }
+      this.x -= this.speed;
+    }, 1000 / 60);
   }
+}
