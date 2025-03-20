3212db7f78ebe08debc92a0f2502c007f7a3c97b
Leugzim Rullani <leugzimrullani@outlook.com>
2025-03-20 11:41:59 +0100

    jsdoc background-object

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,7 +1,29 @@
+/**
+ * Represents a background object that inherits from MovableObject.
+ * These objects serve as background images within the game.
+ * @extends MovableObject
+ */
 class BackgroundObject extends MovableObject {
 
+    /**
+     * The width of the background object.
+     * @type {number}
+     */
     width = 720;
+
+    /**
+     * The height of the background object.
+     * @type {number}
+     */
     height = 480;
+
+    /**
+     * Creates a new instance of BackgroundObject.
+     * Loads the specified image and positions the object on the canvas.
+     *
+     * @param {string} imagePath - The path to the background image.
+     * @param {number} x - The X-position of the background object.
+     */
     constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
